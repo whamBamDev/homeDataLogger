eaf146927722ab15672e1d6a22f537eacffb47d3
whamBamDev <spam.spam.spam@ineson.me>
2017-12-22 00:52:23 +1100

    Prototype of logging functions

---
+++ debug.cpp
@@ -0,0 +1,46 @@
+#include "debug.h"
+
+Debug::Debug (char *logFilename) {
+   _logFlename = logFilename;
+}
+
+void Debug::printf(const char *format, ...) {
+  va_list args;
+  va_start(args, format);
+  logMessage(format, args);
+  va_end(args);
+}
+
+void Debug::printfln(const char *format, ...) {
+  va_list args;
+  va_start(args, format);
+  logMessage(format, args);
+  va_end(args);
+
+  Serial.write('\r');
+  Serial.write('\n');
+}
+
+void Debug::logMessage(const char *format, va_list args) {
+  static char line[80];
+  int len = vsnprintf(line, sizeof(line), format, args);
+  for (char *p = &line[0]; *p; p++) {
+      if (*p == '\n') {
+          Serial.write('\r');
+      }
+      Serial.write(*p);
+  }
+  if (len >= sizeof(line))
+      Serial.write('$');
+}
+
+void Debug::startLogging(void) {
+  _log = true;
+}
+
+void Debug::stopLogging(void) {
+  _log = false;
+}
+
+
+
---
+++ debug.h
@@ -0,0 +1,28 @@
+
+#ifndef _DEBUG_LIB_H_
+#define _DEBUG_LIB_H_
+
+#include <stdio.h>
+
+#include <SD.h>
+#include <Wire.h>
+
+class Debug {
+public:
+    Debug (char *logFilename);
+
+    void printf(const char *format, ...);
+    void printfln(const char *format, ...);
+
+    void startLogging( void);
+    void stopLogging( void);
+
+private:
+    char * _logFlename;
+    bool _log = false;
+
+    void logMessage(const char *format, va_list args);
+};
+
+#endif // _DEBUG_LIB_H_
+
