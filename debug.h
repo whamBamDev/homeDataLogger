f44526a1f3f4fbefd066c0c65f6e2283a98b8162
whamBamDev <spam.spam.spam@ineson.me>
2017-12-26 14:16:36 +1100

    Changes for using constants that are stored in flash memory

--- debug.h
+++ debug.h
@@ -9,17 +9,20 @@
 
 class Debug {
 public:
-    Debug (char *logFilename);
+    Debug (char *logFilename, bool logToConsole);
 
     void printf(const char *format, ...);
+    void printf(const __FlashStringHelper *format, ...);
     void printfln(const char *format, ...);
+    void printfln(const __FlashStringHelper *format, ...);
 
     void startLogging( void);
     void stopLogging( void);
 
 private:
     char * _logFlename;
-    bool _log = false;
+    bool _logEnabled = false;
+    bool _logToConsole = false;
 
     void logMessage(const char *format, va_list args);
 };
